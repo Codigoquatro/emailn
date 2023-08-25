39e84480b38c685c3cd80bcee3adde5906de0a58
Codigoquatro <codigoquatro2022@gmail.com>
2023-08-25 08:57:35 -0300

    criado testes em campaign_test.go

--- go.mod
+++ go.mod
@@ -2,13 +2,13 @@ module emailn
 
 go 1.21.0
 
-require go.starlark.net v0.0.0-20230814145427-12f4cb8177e4
+require (
+	github.com/rs/xid v1.5.0
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,24 +2,65 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_NewCampaign(t *testing.T) {
+var (
+	name     = "Campaign X"
+	content  = "Body"
+	contacts = []string{"alves@gmail.com", "teste@gmail.com"}
+)
+
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
+
 	assert := assert.New(t)
 
-	//Arrange (Preparação): Configura o cenário de teste.
-	name := "Campaign X"
-	content := "Body"
-	contacts := []string{"alves@gmail.com", "teste@gmail.com"}
+	campaign, _ := NewCampaign(name, content, contacts)
 
-	//Act (Execução): Chama o método ou função sob teste.
-	campaign := NewCampaign(name, content, contacts)
-
-	//Assert (Verificação): Verifica se o resultado é o esperado.
-	assert.Equal(campaign.ID, "1")
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
 	assert.Equal(len(campaign.Contacts), len(contacts))
+}
+
+func Test_NewCampaign_IDIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+
+	campaign, _ := NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_CreatedMustBeNow(t *testing.T) {
+	assert := assert.New(t)
+
+	now := time.Now().Add(-time.Minute)
+	campaign, _ := NewCampaign(name, content, contacts)
+
+	assert.Greater(campaign.CreatedOn, now)
+}
+
+func Test_NewCampaign_MustValidateName(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign("", content, contacts)
+
+	assert.Equal("name is required", err.Error())
+}
+
+func Test_NewCampaign_MustValidateContent(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, "", contacts)
+
+	assert.Equal("content is required", err.Error())
+}
+
+func Test_NewCampaign_MustValidateContact(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, content, []string{})
+
+	assert.Equal("contact is required", err.Error())
 }
