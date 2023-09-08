12f0f71136dc06e3eb50e4f69d25ab8c004b3abb
Codigoquatro <codigoquatro2022@gmail.com>
2023-09-08 17:02:52 -0300

    criado endpoint

---
+++ internal/endpoints/handler_error_test.go
@@ -0,0 +1,65 @@
+package endpoints
+
+import (
+	internalerrors "emailn/internal/internalErrors"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_HandlerError_When_Endpoint_Returns_Internal_Error(t *testing.T) {
+	assert := assert.New(t)
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 0, internalerrors.ErrInternal
+	}
+
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusInternalServerError, res.Code)
+	assert.Contains(res.Body.String(), internalerrors.ErrInternal.Error())
+}
+func Test_HandlerError_When_Endpoint_Returns_IDomain_Error(t *testing.T) {
+	assert := assert.New(t)
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 0, errors.New("domain error")
+	}
+
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusBadRequest, res.Code)
+	assert.Contains(res.Body.String(), "domain error")
+}
+
+func Test_HandlerError_When_Endpoint_Returns_Obj_And_Status(t *testing.T) {
+	assert := assert.New(t)
+	type bodyTest struct {
+		ID int
+	}
+	objExpected := bodyTest{ID: 2}
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return objExpected, 201, nil
+	}
+
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusCreated, res.Code)
+	objReturned := bodyTest{}
+	json.Unmarshal(res.Body.Bytes(), &objReturned)
+	assert.Equal(objExpected, objReturned)
+}
